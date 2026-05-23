cb85689f4eaf738f6a18da4450905fc088b66b67
Francisco Alves <140981559+franciscosmt@users.noreply.github.com>
2026-05-23 18:57:37 -0300

    atualizaçao

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
 // IMPORTAR MENU
-
 fetch("menu.html")
-    .then(respomse => response.text())
+    .then(response => response.text())
     .then(data => {
         document.getElementById("menu-container").innerHTML = data;
-
-        // CARREGAR JS DO MENU
-        const script =
-document.createElement("script");
-        script.src = "js/menu.js";
-        document.body.appendChild(script);
-    });
+    })
+    .catch(error => console.log("Erro:", error));
 
 // ANIMAÇÃO DOS CARDS
 window.addEventListener("load", () => {
     const cards = document.querySelectorAll(".card");
-    cards.forEach((caed, index) => {
-        card.style.opacity = 0;
+
+    cards.forEach((card, index) => {
+        card.style.opacity = "0";
         card.style.transform = "translateY(20px)";
+
         setTimeout(() => {
             card.style.transition = "0.5s";
-            card.style.opacity = 1;
+            card.style.opacity = "1";
             card.style.transform = "translateY(0)";
         }, index * 150);
     });
